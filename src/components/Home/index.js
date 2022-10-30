d074ff0604d31488c2abebae33c5c1dcf93d3097
Edmar Jan Gumtang <edmargumtang5@gmail.com>
2022-10-30 12:34:24 +0800

    10th commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -24,18 +24,20 @@ const Home = () => {
             <div className="text-zone">
                 <h1>
                 <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>
+                <span className={`${letterClass} _12`}>i</span>
+                <span className={`${letterClass} _13`}>,</span>
                 <br />
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
+                <span className={`${letterClass} _14`}>I</span>
+                <span className={`${letterClass} _15`}>'</span>
+                <span className={`${letterClass} _16`}>m</span>
                 <img src={LogoTitle} alt="developer" className="Logo" />
                 <AnimatedLetters letterClass={letterClass} 
                 strArray={nameArray} 
-                idx={15} />
+                idx={17} />
                 < br />
                 <AnimatedLetters letterClass={letterClass} 
                 strArray={jobArray} 
-                idx={23} />
+                idx={25} />
                 </h1>
                 <h2>Frontend Developer / React Expert</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
