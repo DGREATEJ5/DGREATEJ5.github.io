53d9e181435bcc6323072a23a9f57a1d4d84cae3
Edmar Jan Gumtang <edmargumtang5@gmail.com>
2022-10-31 17:42:06 +0800

    add portfolio page

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,48 @@
+import './index.scss';
+import './custom.css'
+import React from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import Pic from '../../assets/images/cons.png'
+import { Link } from 'react-router-dom'
+
+const Portfolio = () => {
+
+    const [letterClass, setLetterClass] = React.useState('text-animate')
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
+    return (
+        <>
+        <div className="container portfolio-page">
+            <h1 className="page-title">
+                <AnimatedLetters
+                    letterClass={letterClass}
+                    strArray={"Portfolio".split("")}
+                    idx={15} 
+                />
+            </h1>
+            <div className="cont">
+                <div className="cont-con1">
+                <h6>Under Construction</h6>
+                <p>
+                    This section is under construction, but we are ready to go! I'm preparing something amazing and exciting for you. Contact me for more details.
+                </p>
+                </div>
+                <div className="cont-con2">
+                    <img src={Pic} alt="construction-pic" className="pic-con" />
+                </div>
+            </div>
+            <Link to="/contact" className="flat-button">CONTACT ME</Link>
+        </div>
+
+        <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Portfolio
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoE from '../../assets/images/E-logo4.png'
 import LogoSubtitle from "../../assets/images/logo-sub2.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope, faSuitcase} from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
  
 const Sidebar = () => {
@@ -22,6 +22,9 @@ const Sidebar = () => {
                 </NavLink>
                 <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
                     <FontAwesomeIcon icon={faUser} color="4d4d43" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+                    <FontAwesomeIcon icon={faSuitcase} color="4d4d43" />
                 </NavLink>
                 <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
                     <FontAwesomeIcon icon={faEnvelope} color="4d4d43" />
